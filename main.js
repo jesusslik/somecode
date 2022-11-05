2231e81cb23f992feb5263a25f32e2f7f54ea082
jesusslik <114743601+jesusslik@users.noreply.github.com>
2022-11-05 12:01:06 +0400

    dvmvbkfdnb vldfkin b
    fomesofmesomfoeslmfo;;

---
+++ main.js
@@ -0,0 +1,33 @@
+const canvas = document.getElementById("field");
+const ctx = canvas.getContext("2d");
+canvas.width = 600;
+canvas.height = 400;
+
+let spacePressed = false;
+let angle = 0;
+let hue = 0;
+let frame = 0;
+let score = 0;
+let gamespeed = 2;
+
+let temp = canvas.height - 90;
+
+function animate() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    //ctx.fillRect(10, temp, 50, 50);
+    bird.update();
+    bird.draw();
+    requestAnimationFrame(animate);
+    angle+=0.12;
+}
+
+animate();
+
+window.addEventListener('keydown', function(e) {
+    if(e.code == 'Space') spacePressed = true;
+    temp -= 20;
+});
+
+window.addEventListener('keyup', function(e) {
+    if(e.code == 'Space') spacePressed = false;
+});
